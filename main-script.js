b2b875aa001cc306f742eb809b220391c75aa9f8
Jean Paul Ruiz <jpruiz114@gmail.com>
2018-03-02 17:24:10 -0500

    Update main-script.js

--- main-script.js
+++ main-script.js
@@ -1,35 +1,45 @@
-var countOfPagesScrolled = 0;
+var countOfPagesScrolled = 10;
 var searchPhrase = "Developer";
 
-function scrollDown(height, countOfPagesScrolled){
-	scroll(0, document.body.clientHeight);
-	setTimeout(function(){
-		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
-			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
-		}else
-			return sendRequest();
-	}, 1500);
+function scrollDown(height, countOfPagesScrolled) {
+    scroll(0, document.body.clientHeight);
+    setTimeout(function() {
+        if (height != document.body.clientHeight && countOfPagesScrolled > 0) {
+            scrollDown(document.body.clientHeight, --countOfPagesScrolled);
+        } else
+            return sendRequest();
+    }, 1500);
 }
 
-function sendRequest(){
-	var contactsNum = 0;
-	$.each( $('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext'), 	function() {
-		$(this).each(function() {
-			  $.each(this.attributes, function() {
-			    if(this.specified) {
-				    if (this.name.indexOf("data-ember-action-") >= 0){
-					var allInf = $("#ember"+(this.value-4)).text();
-					if (allInf.indexOf(searchPhrase) >= 0){
-						console.log($("#ember"+(this.value-4)).text());
-						contactsNum++;
-						$(this).click();
-					}
-				    }
-			    }
-			  });
-			});
-	});
-	console.log('Just added contacts: ' + contactsNum);
+function sendRequest() {
+    var contactsNum = 0;
+    
+	$.each($('.mn-person-info__occupation'), function() {
+        $(this).each(function() {
+			var currentElement = $(this);
+			console.dir(currentElement);
+			
+			var content = $.trim($(currentElement).text());
+			console.log('content' + ' = ' + content);
+			
+			if (content.indexOf(searchPhrase) !== -1) {
+				console.log('found one match');
+				
+				var parent3UP = $(this).parent().parent().parent();
+				console.dir(parent3UP);
+				
+				// find child where attribute equals data-control-name
+				var inviteButton = $(parent3UP).find("[data-control-name='" + 'invite' + "']");
+				console.dir(inviteButton);
+				
+				$(inviteButton).click();
+				contactsNum++;
+			}
+        });
+    });
+	
+    console.log('Just added contacts: ' + contactsNum);
 }
 
 scrollDown(document.body.clientHeight, countOfPagesScrolled);
+
